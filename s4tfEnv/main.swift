b345b784c106d19ed2750e97c4c44b3d0f299288
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2020-05-15 04:15:18 +0530

    Initial Commit

---
+++ s4tfEnv/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  s4tfEnv
+//
+//  Created by Ayush Agrawal on 15/05/20.
+//  Copyright © 2020 Ayush Agrawal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
