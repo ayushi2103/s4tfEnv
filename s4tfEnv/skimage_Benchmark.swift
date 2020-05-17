4d802520304c16cceda3e832a1ed2f8655ed3ea9
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2020-05-17 23:28:28 +0530

    correct naming from scipy to skimage

--- s4tfEnv/skimage_Benchmark.swift
+++ s4tfEnv/skimage_Benchmark.swift
@@ -1,5 +1,5 @@
 //
-//  scipy_benchmark.swift
+//  skimage_benchmark.swift
 //  s4tfEnv
 //
 //  Created by Ayush Agrawal on 17/05/20.
@@ -17,40 +17,40 @@ let skio = Python.import("skimage.io")
 let sktransform = Python.import("skimage.transform")
 let skutil = Python.import("skimage.util")
 
-public func scipyResizeOperation() {
+public func skimageResizeOperation() {
     img = skio.imread(path)
     img = sktransform.resize(img, [32, 32])
 }
 
-public func scipySaveOperation() {
+public func skimageSaveOperation() {
     img = skio.imread(path)
-    let fname = "/Users/ayush517/Downloads/scipySaved.jpeg"
+    let fname = "/Users/ayush517/Downloads/skimageSaved.jpeg"
     img = skio.imsave(fname, img)
 }
 
 
-public func scipyTransposeOperation() {
+public func skimageTransposeOperation() {
     img = skio.imread(path)
     img = sktransform.rotate(img, 90)
 }
 
-public func scipyCropOperation() {
+public func skimageCropOperation() {
     img = skio.imread(path)
     let h = Float(img.shape[0])! / 4
     let w = Float(img.shape[1])! / 4
     img = skutil.crop(img, [[h, h], [w, w], [0, 0]])
 }
 
-public func scipyBenchmark() {
+public func skimageBenchmark() {
     print("Resize Operation")
-    benchmark(scipyResizeOperation)
+    benchmark(skimageResizeOperation)
     print(" ")
     print("Save Operation")
-    benchmark(scipySaveOperation)
+    benchmark(skimageSaveOperation)
     print(" ")
     print("Transpose Operation")
-    benchmark(scipyTransposeOperation)
+    benchmark(skimageTransposeOperation)
     print(" ")
     print("Crop Operation")
-    benchmark(scipyCropOperation)
+    benchmark(skimageCropOperation)
 }
